0107096588816aeac406cf47c0682e1a518c8195
Yonatan Kogan <yonatan@tockhq.com>
2017-07-25 20:35:10 -0500

    Make better use of redux

--- web/src/js/components/map/MarkerWithInfoWindow.jsx
+++ web/src/js/components/map/MarkerWithInfoWindow.jsx
@@ -6,27 +6,26 @@ class MarkerWithInfoWindow extends React.Component {
   constructor(props) {
     super(props);
     autoBind(this);
-
-    this.state = {
-      isInfoWindowOpen: false,
-    };
   }
   shouldComponentUpdate(/* nextProps, nextState */) {
     return true;
   }
 
   handleMarkerClick() {
-    this.setState({
-      isInfoWindowOpen: !this.state.isInfoWindowOpen,
-    });
+    this.props.onClick(this.props.id);
   }
 
   render() {
     return (
       <Marker {...this.props} onClick={this.handleMarkerClick}>
-        {this.state.isInfoWindowOpen &&
+        {this.props.isSelected &&
           <InfoWindow onCloseClick={this.handleMarkerClick}>
-            <div>{this.props.title}</div>
+            <div>
+              <div>{this.props.name}</div>
+              <div>{this.props.address}</div>
+              <div>{this.props.phone}</div>
+              <div>{this.props.website}</div>
+            </div>
           </InfoWindow>
         }
       </Marker>
@@ -35,7 +34,13 @@ class MarkerWithInfoWindow extends React.Component {
 }
 
 MarkerWithInfoWindow.propTypes = {
-  title: React.PropTypes.string.isRequired,
+  address: React.PropTypes.string.isRequired,
+  id: React.PropTypes.string.isRequired,
+  isSelected: React.PropTypes.bool.isRequired,
+  name: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func.isRequired,
+  phone: React.PropTypes.string.isRequired,
+  website: React.PropTypes.string.isRequired,
 };
 
 export default MarkerWithInfoWindow;
--- web/src/js/routes/MapRoute.jsx
+++ web/src/js/routes/MapRoute.jsx
@@ -1,49 +1,20 @@
+/* eslint-disable react/jsx-handler-names */
 import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
 import shallowCompare from 'react-addons-shallow-compare';
 import { default as MarkerClusterer } from 'react-google-maps/lib/addons/MarkerClusterer';
+
+import { setSelectedMarker } from 'actions/MapActions';
 
 import GoogleMapWrapper from 'components/map/GoogleMapWrapper';
 import MarkerWithInfoWindow from 'components/map/MarkerWithInfoWindow';
 
-import naiPartners from 'static/naiPartners.csv';
-
 const DOWNTOWN_CHICAGO_LAT_LNG = { lat: 41.8781, lng: -87.6298 };
 
 class MapRoute extends React.Component {
-  constructor(props) {
-    super(props);
-    this.geocoder = new window.google.maps.Geocoder();
-
-    this.state = {
-      markers: [],
-    };
-  }
-  
-  componentDidMount() {
-    this.getMarkers();
-  }
-
   shouldComponentUpdate(nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState);
-  }
-
-  getMarkers() {
-    const markers = _.map(naiPartners, (partner) => {
-      return {
-        position: {
-          lat: partner.lat,
-          lng: partner.lng,
-        },
-        key: partner.address,
-        title: partner.address,
-        defaultAnimation: 2,
-      };
-    });
-    console.debug(markers);
-    this.setState({
-      markers,
-    });
   }
 
   render() {
@@ -64,9 +35,12 @@ class MapRoute extends React.Component {
             averageCenter={ true }
             enableRetinaIcons={ true }
             gridSize={ 60 }>
-            {_.map(this.state.markers, (marker) => {
+            {_.map(this.props.markers, (marker) => {
               return (
-                <MarkerWithInfoWindow {...marker} />
+                <MarkerWithInfoWindow
+                  {...marker}
+                  isSelected={marker.id === this.props.selectedMarker}
+                  onClick={this.props.setSelectedMarker} />
               );
             })}
           </MarkerClusterer>
@@ -76,6 +50,21 @@ class MapRoute extends React.Component {
   }
 }
 
-MapRoute.propTypes = {};
+MapRoute.propTypes = {
+  markers: React.PropTypes.arrayOf(React.PropTypes.object), // TODO: Better prop types
+  setSelectedMarker: React.PropTypes.func.isRequired,
+  selectedMarker: React.PropTypes.string,
+};
 
-export default MapRoute;
+const actions = {
+  setSelectedMarker,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    markers: state.map.markers,
+    selectedMarker: state.map.selectedMarker,
+  };
+};
+
+export default connect(mapStateToProps, actions)(MapRoute);
